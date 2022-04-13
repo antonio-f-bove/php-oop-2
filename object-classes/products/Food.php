1337ddf3d556b470aa907bc1da6bdde3fa25d3cc
Antonio Bove <antoniofbove@gmail.com>
2022-04-13 15:43:07 +0200

    - Product.php: added canExpire trait - Food.php: structure

--- object-classes/products/Food.php
+++ object-classes/products/Food.php
@@ -3,6 +3,15 @@
 require_once __DIR__ . '/Product.php';
 
 class Food extends Product {
-  public $exp_date;
-  public
+
+  use canExpire;
+
+  public $weight;
+
+  function __construct($_name, $_brand, $_price, $_exp_date, $_weight)  {
+    parent::__construct($_name, $_brand, $_price);
+    
+    $this->exp_date = $this->checkDateFormat($_exp_date);
+    $this->weight = $_weight;
+  }
 }
--- object-classes/products/Product.php
+++ object-classes/products/Product.php
@@ -1,12 +1,38 @@
 <?php
-
-/* 
-Ho scelto di fare una classe astratta per includere
-*/
 abstract class Product {
 
   public $name;
   public $brand;
   public $price;
   public $species = []; // for what species of animal?
+
+  public $description;
+
+  function __construct($_name, $_brand, $_price) {
+    $this->name = $_name;
+    $this->brand = $_brand;
+    $this->rice = $_price;
+  }
+
+  public function setDescription(string $_desc) {
+    // TODO controlli?
+    $this->description = $_desc;
+  }
+}
+
+// ------------------------------------------------------
+
+trait canExpire {
+  public $exp_date;
+  
+  public function isExpired() {
+    return date($this->exp_date) > date('m/y');
+  }
+
+  private function checkDateFormat($_date) {
+    $timestamp = strtotime($_date);
+    
+    if (!$timestamp) throw new Exception('Invalid date input.');
+    return date('m/y', $timestamp);
+  }
 }
